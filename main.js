600a9d3111e9a2d20d0a7b7e5b5daef22ef995ce
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-02-24 13:50:09 +0300

    fetching an api for weather forecast from openweathermap

--- main.js
+++ main.js
@@ -5,5 +5,6 @@ const windSpeed = document.querySelector(".wind-speed")
 
 
 btn.addEventListener("click", () =>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=93dee87a25439f60c78b5bec94880922`)
     
 });
