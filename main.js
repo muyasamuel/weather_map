89e813f06beb6568bf8f6dfe415e376c8854bf5f
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-02-24 13:56:29 +0300

    Transforming the response data from api into json format then displaying it

--- main.js
+++ main.js
@@ -6,5 +6,8 @@ const windSpeed = document.querySelector(".wind-speed")
 
 btn.addEventListener("click", () =>{
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=93dee87a25439f60c78b5bec94880922`)
+    .then(Response => Response.json())
+    .then(data => console.log(data))
+
     
 });
