c2ed83ffa6e57cb11bc793b5740d74b386d22c7e
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-02-24 13:45:06 +0300

    Getting all dom elements required

---
+++ main.js
@@ -0,0 +1,4 @@
+const btn = document.querySelector(".btn")
+const inputValue = document.querySelector(".inputValue")
+const desc = document.querySelector(".desc")
+const windSpeed = document.querySelector(".wind-speed")
