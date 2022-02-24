8c5dd229137d171a8023b81be65940a45e1ab3d1
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-02-24 13:47:15 +0300

    Putting an event listener to the button when it is clicked

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ const btn = document.querySelector(".btn")
 const inputValue = document.querySelector(".inputValue")
 const desc = document.querySelector(".desc")
 const windSpeed = document.querySelector(".wind-speed")
+
+
+btn.addEventListener("click", () =>{
+    
+});
