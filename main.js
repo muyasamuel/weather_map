3809f37b89a954fb1d3c83ba891ab145dfbc2bfd
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-02-24 13:59:34 +0300

    Getting the values of temp, decription and windspeed from the weather data

--- main.js
+++ main.js
@@ -7,7 +7,17 @@ const windSpeed = document.querySelector(".wind-speed")
 btn.addEventListener("click", () =>{
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=93dee87a25439f60c78b5bec94880922`)
     .then(Response => Response.json())
-    .then(data => console.log(data))
+    .then(data =>{
+        console.log(data)
+
+        let temp = data.main.temp;
+        let description = data.weather[0].description;
+        let windSpeed = data.wind.speed;
+        let icon = data.weather[0].icon;
+
+
+
+    })
 
     
 });
